e2a113bfb04af542ea2f8e85971ba76f40a72619
D22 <aguilara42@ninja23.d22.lan>
2015-10-01 06:12:24 -0500

    first commit

---
+++ src/Aldo_Aguilar_3_challengeZero.java
@@ -0,0 +1,32 @@
+
+public class Aldo_Aguilar_3_challengeZero {
+	public static void main(String args[]){
+		
+	}
+	
+	static boolean divisibleByTwo (int number){
+		if (number % 2 == 0){
+			return true;
+		}
+		
+		else{
+			return false;
+		}
+
+	}
+
+	static boolean divisibleByThree(int number){
+		
+	}
+	
+	static boolean divisibleByFive(int number){
+
+	}
+	
+	static boolean divisibleByEleven(int number){
+
+	}
+	
+		
+
+}
