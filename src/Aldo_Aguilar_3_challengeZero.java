849aeaf808240b829df3294acc471bd8e058e140
D22 <aguilara42@ninja23.d22.lan>
2015-10-02 10:28:32 -0500

    Done

--- src/Aldo_Aguilar_3_challengeZero.java
+++ src/Aldo_Aguilar_3_challengeZero.java
@@ -1,32 +1,88 @@
-
 public class Aldo_Aguilar_3_challengeZero {
-	public static void main(String args[]){
-		
-	}
-	
-	static boolean divisibleByTwo (int number){
-		if (number % 2 == 0){
-			return true;
-		}
-		
-		else{
-			return false;
-		}
+    public static void main(String args[]){
+        for (int i = 1; i < 500; i ++){
+            
+            if (isDivisibleByTwo(i) & isDivisibleByThree(i)){
+                System.out.println( i+" Chisme");
+            }
+            
+            else if (isDivisibleByFive(i) & isDivisibleByThree(i)){
+                System.out.println( i+" Old School");
+            }
+            
+            else if (isDivisibleByFive(i) & isDivisibleByThree(i) & isDivisibleByTwo(i)){
+                System.out.println( i+" Team Building");
+            }
+            
+            else{
+                if (isDivisibleByTwo(i) & isDivisibleByEleven(i) ){
+                    System.out.println( i+" Ninja");
+                }
+            
+                else if (isDivisibleByTwo(i)){
+                    System.out.println( i+" Davis");
+                }
+            
+                if(isDivisibleByThree(i) & isDivisibleByEleven(i)){
+                    System.out.println( i+" Herr Direktor");
+                }
+                else if (isDivisibleByThree(i)){
+                    System.out.println( i+" Claughton");
+                }
+            
+                if(isDivisibleByFive(i) & isDivisibleByEleven(i)){
+                    System.out.println( i+" Animate This");
+                }
+            
+                else if (isDivisibleByFive(i)){
+                    System.out.println( i+" Vidal");
+                }
+            }
+            
+        }
+    }
+    
+    static boolean isDivisibleByTwo (int number){
+        if (number % 2 == 0){
+            return true;
+        }
+        
+        else{
+            return false;
+        }
 
-	}
+    }
 
-	static boolean divisibleByThree(int number){
-		
-	}
-	
-	static boolean divisibleByFive(int number){
-
-	}
-	
-	static boolean divisibleByEleven(int number){
-
-	}
-	
-		
+    static boolean isDivisibleByThree(int number){
+        if (number % 3 ==0){
+            return true;
+        }
+        
+        else{
+            return false;
+        }
+    }
+    
+    static boolean isDivisibleByFive(int number){
+        if (number % 5 ==0){
+            return true;
+        }
+        
+        else{
+            return false;
+        }
+    }
+    
+    static boolean isDivisibleByEleven(int number){
+        if (number % 11 ==0){
+            return true;
+        }
+        
+        else{
+            return false;
+        }
+    }
+    
+        
 
 }
